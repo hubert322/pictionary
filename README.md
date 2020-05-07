326e4328a9ee05c30b8f8e5d9056611564f197e2
hubert322 <huberth@umich.edu>
2020-05-07 18:21:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Skribbl
+## Live at: [huberthung.me](https://huberthung.me)
+## This project is created using React, Flask, and MongoDB
---
+++ static/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center App">
+      Skribbl
+    </div>
+  );
+}
+
+export default App;
