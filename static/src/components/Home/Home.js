9fedff9dba0d2257443dbd7252ba84b9639686cb
hubert322 <huberth@umich.edu>
2020-05-11 15:39:18 +0800

    change file name

--- static/src/components/Home/Home.js
+++ static/src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
-import { joinGame, newGame, getUid } from "./HomeApi";
+import { joinGame, newGame, getUid } from "./HomeApiSocket";
 import { useLocalStorage } from "../../utils/hooks";
 import "../App/App.css";
 import "./Home.css";
