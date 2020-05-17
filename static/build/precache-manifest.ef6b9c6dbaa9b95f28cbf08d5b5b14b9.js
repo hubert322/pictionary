c4aa1aefb8651d0cff3ef3ee6b3078c1deabb322
hubert322 <huberth@umich.edu>
2020-05-17 15:02:52 +0800

    build

---
+++ static/build/precache-manifest.ef6b9c6dbaa9b95f28cbf08d5b5b14b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f21e06194257be29150a63c5fb87264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b7c1bb1044ef615265",
+    "url": "/static/css/2.8b8befd2.chunk.css"
+  },
+  {
+    "revision": "3b95d7acc2a8cad1e975",
+    "url": "/static/css/main.11f073e4.chunk.css"
+  },
+  {
+    "revision": "27b7c1bb1044ef615265",
+    "url": "/static/js/2.fe800865.chunk.js"
+  },
+  {
+    "revision": "3b95d7acc2a8cad1e975",
+    "url": "/static/js/main.93aa3ba2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
