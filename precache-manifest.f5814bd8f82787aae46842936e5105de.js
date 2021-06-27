34166ffb74c6958310bd6dedef8629b35d329158
Hubert Hung <hubert322@gmail.com>
2021-06-27 15:43:43 -0400

    Updates

---
+++ precache-manifest.f5814bd8f82787aae46842936e5105de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46dea6f0cfee5e5dbf6d309f64c9fcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7389f501de62a44c495d",
+    "url": "/static/css/2.13ab85ea.chunk.css"
+  },
+  {
+    "revision": "8158bd860a165709d95c",
+    "url": "/static/css/main.22a05b41.chunk.css"
+  },
+  {
+    "revision": "7389f501de62a44c495d",
+    "url": "/static/js/2.70e5a247.chunk.js"
+  },
+  {
+    "revision": "8158bd860a165709d95c",
+    "url": "/static/js/main.0227f2e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
